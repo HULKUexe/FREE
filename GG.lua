01cc9cc06920b85b2289f53f5df91abb25dfea21
HULKUexe <94535468+HULKUexe@users.noreply.github.com>
2021-12-11 09:03:10 +0700

    Update GG.lua

--- GG.lua
+++ GG.lua
@@ -3958,6 +3958,7 @@ Tab1:Toggle("Auto Farm Level",false,function(t)
                 CheckLv()
                 Quest()
                 --Abandon()
+                EquipWeapon(_G.SelectToolWeapon)
                 AutoHaki()
                 game.Players.LocalPlayer.Character.Humanoid:ChangeState(11) 
                 Tp()
@@ -3997,45 +3998,27 @@ end)
 end)
 end)
 
-
-
-
-Tab1:Button("FPS Boost",function(t)
-    local decalsyeeted = true -- Leaving this on makes games look shitty but the fps goes up by at least 20.
-    local g = game
-    local w = g.Workspace
-    local l = g.Lighting
-    local t = w.Terrain
-    t.WaterWaveSize = 0
-    t.WaterWaveSpeed = 0
-    t.WaterReflectance = 0
-    t.WaterTransparency = 0
-    l.GlobalShadows = false
-    l.FogEnd = 9e9
-    l.Brightness = 0
-    settings().Rendering.QualityLevel = "Level01"
-    for i, v in pairs(g:GetDescendants()) do
-        if v:IsA("Part") or v:IsA("Union") or v:IsA("CornerWedgePart") or v:IsA("TrussPart") then 
-            v.Material = "Plastic"
-            v.Reflectance = 0
-        elseif v:IsA("Decal") or v:IsA("Texture") and decalsyeeted then
-            v.Transparency = 1
-        elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
-            v.Lifetime = NumberRange.new(0)
-        elseif v:IsA("Explosion") then
-            v.BlastPressure = 1
-            v.BlastRadius = 1
-        elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
-            v.Enabled = false
-        elseif v:IsA("MeshPart") then
-            v.Material = "Plastic"
-            v.Reflectance = 0
-            v.TextureID = 10385902758728957
+Wapon = {}
+for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do  
+    if v:IsA("Tool") then
+        table.insert(Wapon ,v.Name)
+    end
+end
+ local SelectWeapona = Tab1:Dropdown("Select Weapon",Wapon,function(Value)
+    _G.SelectToolWeapon = Value
+    SelectToolWeaponOld = Value
+end)
+Tab1:Button("Refresh Weapon",function()
+    SelectWeapona:Clear()
+    Wapon = {}
+    for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do  
+        if v:IsA("Tool") then
+            SelectWeapona:Add(v.Name)
         end
     end
-    for i, e in pairs(l:GetChildren()) do
-        if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
-            e.Enabled = false
+    for i,v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do  
+        if v:IsA("Tool") then
+            SelectWeapona:Add(v.Name)
         end
     end
 end)
